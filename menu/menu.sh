3179e844b73cb71596369b9c266caca05045d561
retrivp <sahalirsyad7@gmail.com>
2024-08-03 20:51:31 +0700

    menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -376,7 +376,7 @@ clear
 clear && clear && clear
 clear;clear;clear
 echo -e " $COLOR1╔══════════════════════════════════════════════════════════╗${NC}"
-echo -e " $COLOR1║${NC}${COLBG1}               ${WH} .::::. RETRI PROJECT .::::.                         ${NC}$COLOR1║ $NC"
+echo -e " $COLOR1║${NC}${COLBG1}         ${WH} .::::. RETRI PROJECT .::::.${NC}$COLOR1║ $NC"
 echo -e " $COLOR1╚══════════════════════════════════════════════════════════╝${NC}"
 echo -e " $COLOR1╔══════════════════════════════════════════════════════════╗${NC}"
 echo -e " $COLOR1║$NC${WH}• OS            ${COLOR1}: ${WH}$MODEL2${NC}"
